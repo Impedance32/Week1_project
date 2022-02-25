a71cc11c2a26461ed5e50fb4569b853da00eb117
Abhishek Aman <99893815+Impedance32@users.noreply.github.com>
2022-02-26 00:42:24 +0530

    Add files via upload

---
+++ plot1.r
@@ -0,0 +1,9 @@
+
+
+data <- read.table("household_power_consumption.txt")
+datause <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.copy(png,file="plot1.png")
+dev.off()
+
+
